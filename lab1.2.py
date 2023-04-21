108588cbb5213911cf002302515adcaddc7f263b
Akaaniss <annaismonserrat.skz@gmail.com>
2023-04-21 13:20:14 -0400

    5?

--- lab1.2.py
+++ lab1.2.py
@@ -1,18 +1,18 @@
 import random
 
-fila=int(input("Ingrese la cantidad de filas:\n"))
+filas=int(input("Ingrese la cantidad de filas:\n"))
 columnas=int(input("Ingrese la cantidad de columnas:\n"))
 
 escalar=int(input("Ingrese un escalar:"))
 matriz=[]
 
-for i in range(fila):
+for i in range(filas):
     fila=[]
     for j in range(columnas):
         fila.append(random.randint(0,10))
     matriz.append(fila)
 
-for i in range(fila):
+for i in range(filas):
     for j in range(columnas):
         matriz[i][j]*=escalar
 
--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import random
-
+#creamos las filas
 fila1=int(input("Ingrese la cantidad de filas para la primera matriz:\n"))
 columna1=int(input("Ingrese la cantidad de columnas para la primera matriz:\n"))
 
@@ -9,23 +9,25 @@ columna2=int(input("ingrese la cantidad de columnas para la segunda matriz:\n"))
 matriz1=[[random.randint(0,5) for j in range(columna1)for i in range(fila1)]]
 matriz2=[[random.randint(0,5) for j in range(columna2) for i in range(fila2)]]
 
+#imprimimos las matrices
 print("Matriz 1:")
-for fila1 in matriz1:
+for fila in matriz1:
     print(fila1)
 
 print ("Matriz 2:")
-for fila2 in matriz2:
+for fila in matriz2:
     print(fila2)
 
 if fila1 == fila2 and columna1 == columna2:
-    matrizResultante= [[matriz1[i][j] + matriz2[i][j] for j in range(columna1)] for i in range(fila1)]
+    matriz_resultante = [[matriz1[i][j] + matriz2[i][j] for j in range(columna1)] for i in range(fila1)]
 
 else:
     print("las dimensiones de las matrices no coinciden, por tanto no se pueden sumar")
     exit()
 
-print("El resultado de la suma de Matricez es:")
-for fila in matrizResultante:
+
+print("El resultado de la suma de Matrices2 es:")
+for fila in matriz_resultante:
     print(fila)
 
 
@@ -39,7 +41,7 @@ for fila3 in matriz3:
 
 
 if fila1 == fila3 and columna1==columna3:
-    matriz_final = [[matrizResultante[i][j] - matriz3[i][j] for j in range(columna1)] for i in range(fila1)]
+    matriz_final = [[matriz_resultante[i][j] - matriz3[i][j] for j in range(columna1)] for i in range(fila1)]
 
 else:
     print("Las dimensiones de las matrices no coinciden y no se pueden restar")
